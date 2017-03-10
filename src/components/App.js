3f3281191586324286656937d6e50621250b921e
Harsheet Bamrah <harsheet.bamrah@pacificmags.com.au>
2017-03-10 14:46:11 +1100

    React and express understanding

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Header from './Header'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (props) => {
+  return (
+  <div className="App">
+    <Header message="Test App" />
+    <div>
+    ...
+    </div>
+  </div>
+  )
+}
+
+export default App;
